6b9f9a864615606ad1fd663c74a341670ac105e5
s183578 <113361740+s183578@users.noreply.github.com>
2022-11-26 11:31:05 +0100

    Add files via upload

---
+++ A2_FutureBIM/main.py
@@ -0,0 +1,5 @@
+# Hi :) if just starting out - probably should start with the duplex.py this main.py references models that aren't shared on this repo
+
+import HTMLBuild as hb
+
+hb.modelLoader("22_03_01")
